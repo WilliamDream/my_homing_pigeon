7d41c1448e8aa92ab1290491e0ecfdf14577d20c
williamdream <3339642010@qq.com>
2019-01-09 23:19:39 +0800

    nettydemo

---
+++ nettydemo1/src/main/java/com/william/demo1/NettyServer.java
@@ -0,0 +1,66 @@
+package com.william.demo1;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+public class NettyServer {
+    private static final String IP = "127.0.0.1";
+    private static final int port = 8000;
+    private static final int BIZGROUPSIZE =  Runtime.getRuntime().availableProcessors() * 2;
+
+    private static final int BIZTHREADSIZE = 100;
+
+    private static final EventLoopGroup bossGroup = new NioEventLoopGroup(BIZGROUPSIZE);
+    private static final EventLoopGroup workGroup = new NioEventLoopGroup(BIZTHREADSIZE);
+
+    public static void start() throws Exception {
+
+        ServerBootstrap serverBootstrap = initServerBootstrap();
+
+        ChannelFuture channelFuture = serverBootstrap.bind(IP, port).sync();
+
+        channelFuture.channel().closeFuture().sync();
+        System.out.println("server start");
+
+    }
+
+    private static ServerBootstrap initServerBootstrap() {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<Channel>() {
+
+
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,0,4,0,4));
+                        pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                        pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+                        pipeline.addLast(new ServerHandler());
+                    }
+                });
+        return serverBootstrap;
+    }
+
+    protected static void shutdown(){
+        workGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("启动服务到NettyServer");
+        NettyServer.start();
+    }
+}
